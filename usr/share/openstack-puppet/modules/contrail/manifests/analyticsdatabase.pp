8cd7297fdf8bff9fa282af8702cb3986502c54e2
michaelhenkel <mhenkel@juniper.net>
2016-11-22 15:23:30 +0100

    changed

--- usr/share/openstack-puppet/modules/contrail/manifests/analyticsdatabase.pp
+++ usr/share/openstack-puppet/modules/contrail/manifests/analyticsdatabase.pp
@@ -9,14 +9,15 @@
 #
 class contrail::analyticsdatabase (
   $package_name = $contrail::params::database_package_name,
+  $analyticsdatabase_params = $analyticsdatabase_params,
 ) inherits contrail::params {
 
   anchor {'contrail::analyticsdatabase::start': } ->
   #class {'::contrail::database::install': } ->
   class {'::contrail::analyticsdatabase::config': 
-    database_nodemgr_config => $analyticsdatabase['database_nodemgr_config'],
-    cassandra_servers       => $analyticsdatabase['cassandra_servers'],
-    cassandra_ip            => $analyticsdatabase['host_ip'],
+    database_nodemgr_config => $analyticsdatabase_params['database_nodemgr_config'],
+    cassandra_servers       => $analyticsdatabase_params['cassandra_servers'],
+    cassandra_ip            => $analyticsdatabase_params['host_ip'],
   } ~>
   class {'::contrail::analyticsdatabase::service': }
   anchor {'contrail::analyticsdatabase::end': }
